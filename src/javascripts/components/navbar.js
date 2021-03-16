0db1d5a0c3ff4fe72e682e6c7ec9050d2eb28c2a
elliewalkersm <elliewalkersm@gmail.com>
2021-03-16 12:02:41 -0500

    navbar

--- src/javascripts/components/navbar.js
+++ src/javascripts/components/navbar.js
@@ -1,13 +1,19 @@
 const navbar = () => {
   const domString = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item" id="sign-in-btn">
-      </li>
+      <div class="navbar-nav">
+        <a class="nav-link active" href="#">Menu<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Ingredients</a>
+        <a class="nav-link" href="#">Staff</a>
+        <a class="nav-link" href="#">Seating</a>
+        <a class="nav-link" href="#">Reservations</a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item" id="login-btn"></li> 
+      </div>
     </div>
   </div>
 </nav>`;
