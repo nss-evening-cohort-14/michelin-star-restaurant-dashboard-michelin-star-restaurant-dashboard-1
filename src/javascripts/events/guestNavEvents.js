a023f1b278b3c0fbfcd201503c0f09bfa4a0e8ba
Rob Cole <robertncole1@gmail.com>
2021-03-16 20:15:21 -0500

    added in reservations to guestNavEvents

--- src/javascripts/events/guestNavEvents.js
+++ src/javascripts/events/guestNavEvents.js
@@ -1,9 +1,15 @@
+import showReservations from '../components/reservations';
+import getReservations from '../helpers/data/reservationData';
 import getIngredients from '../ingredients/ingredientsData';
 import showIngredients from '../ingredients/showIngredients';
 
 const guestNavEvents = () => {
   document.querySelector('#navigation').addEventListener('click', (e) => {
     // Events for Navbar, READ only
+
+    document.querySelector('#nav-reservations').addEventListener('click', () => {
+      getReservations().then((reservations) => showReservations(reservations));
+    });
 
     // Click event for Ingredients
     if (e.target.id.includes('nav-ingredients')) {
