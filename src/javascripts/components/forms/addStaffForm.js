5adfa7accec7678962e282e9eb17a67e49b687df
Sean <jb@JBs-MacBook-Pro.local>
2021-03-19 16:52:30 -0500

    fixed labels

--- src/javascripts/components/forms/addStaffForm.js
+++ src/javascripts/components/forms/addStaffForm.js
@@ -2,7 +2,7 @@ const addStaffForm = () => {
   document.querySelector('#modal-body').innerHTML = `
     <form id="add-staff-form" class="mb-4">
       <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">First Name</label>
+        <label for="new-first-name" class="form-label">First Name</label>
         <input type="text" class="form-control" id="new-first-name" placeholder="First Name">
       </div>
       <div class="mb-3">
@@ -10,15 +10,15 @@ const addStaffForm = () => {
         <input type="text" class="form-control" id="new-last-name" placeholder="Last Name">
       </div>
       <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Position</label>
+        <label for="new-last-name" class="form-label">Position</label>
         <input type="text" class="form-control"  id="new-position" placeholder="Position">
       </div>
       <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Image</label>
+        <label for="new-image-url" class="form-label">Image</label>
         <input type="url" class="form-control" id="new-image-url" placeholder="URL">
       </div>
       <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Bio</label>
+        <label for="new-staff-bio" class="form-label">Bio</label>
         <textarea class="form-control" id="new-staff-bio" rows="3"></textarea>
       </div>
       <button type="submit" id="submit-staff" class="btn btn-primary">Add Staff</button>
