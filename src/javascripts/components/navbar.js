4fcac4a650ae1a6a7fd1b8a8f416c08c11cdd34d
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-04-12 18:38:41 -0500

    styling and prettiness :)

--- src/javascripts/components/navbar.js
+++ src/javascripts/components/navbar.js
@@ -1,20 +1,20 @@
 const navbar = () => {
-  const domString = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  const domString = `<nav class="navbar navbar-expand-lg justify-content-center">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <div class="navbar-nav">
-        <a class="nav-link active" id="nav-menu" href="#">Menu</a>
+        <a class="nav-link active" href="#" id="nav-home">Home</a>
+        <a class="nav-link" id="nav-menu" href="#">Menu</a>
         <a class="nav-link" id="nav-ingredients" href="#">Ingredients</a>
         <a class="nav-link" id="nav-staff" href="#">Staff</a>
         <a class="nav-link" id="nav-seating" href="#">Seating</a>
         <a class="nav-link" id="nav-reservations" href="#">Reservations</a>
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item" id="login-btn"></li> 
       </div>
     </div>
+    <div class="nav-item" id="login-btn"></div> 
   </div>
 </nav>`;
   document.querySelector('#navigation').innerHTML = domString;
--- src/javascripts/components/reservations/singleReservation.js
+++ src/javascripts/components/reservations/singleReservation.js
@@ -6,13 +6,6 @@ const singleReservation = (item) => {
         <h2 class="modal-header">${item.name} — Table #</h2>
         <p class="modal-header">Party of ${item.party_size}</p>
         <p class="modal-header">${item.day_of_week}, ${item.date} at ${item.time}</p>
-      <div id="reservationStaff">
-        <h2>Assigned Staff</h2>
-          <p>Host: ${item.job_title || 'Not Assigned'}</p>
-          <p>Server: ${item.job_title || 'Not Assigned'}</p>
-          <p>Busser: ${item.job_title || 'Not Assigned'}</p>
-          <p>Bartender: ${item.job_title || 'Not Assigned'}</p>
-        </div>
       </div>
       <div class="modal-footer">
         <p>${item.notes}</p>
@@ -20,5 +13,4 @@ const singleReservation = (item) => {
     </div>
   </div>`;
 };
-
 export default singleReservation;
