d52eb0ca4bba4b91b736d749985c6e2045494e16
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-04-14 19:23:22 -0500

    Update reservations.js

--- src/javascripts/components/reservations/reservations.js
+++ src/javascripts/components/reservations/reservations.js
@@ -25,13 +25,15 @@ const showLoginReservations = (array) => {
     document.querySelector('#view').innerHTML += `
     <div class="card m-3" style="width: 20rem;">
     <div class="card-body">
-    <h3 class="card-title text-capitalize" id="res-title--${item.firebaseKey}">${item.name}</h3>
-    </br>
+      <div class="card-header">
+        <a href="#" id="res-link"><h5 id="res-title--${item.firebaseKey}" class="card-title mt-5">${item.name}</h5></a>
+      </div>
     <div class="card-body">
       <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#formModal" id="edit-res-btn--${item.firebaseKey}">Edit</button>
       <button type="button" class="btn btn-danger" id="delete-res--${item.firebaseKey}">Delete</button></li>
     </div>
-  </div>`;
+  </div>
+</div>`;
   });
 };
 
