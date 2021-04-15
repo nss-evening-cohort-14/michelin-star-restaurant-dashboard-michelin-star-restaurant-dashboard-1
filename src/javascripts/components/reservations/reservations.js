100c4349da441ec6540f6a7f6ec0cfd72a385283
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-04-14 19:11:26 -0500

    Update reservations.js

--- src/javascripts/components/reservations/reservations.js
+++ src/javascripts/components/reservations/reservations.js
@@ -28,7 +28,7 @@ const showLoginReservations = (array) => {
     <h3 class="card-title text-capitalize" id="res-title--${item.firebaseKey}">${item.name}</h3>
     </br>
     <div class="card-body">
-      <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#formModal" id="update-res--${item.firebaseKey}">Edit</button>
+      <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#formModal" id="edit-res-btn--${item.firebaseKey}">Edit</button>
       <button type="button" class="btn btn-danger" id="delete-res--${item.firebaseKey}">Delete</button></li>
     </div>
   </div>`;
